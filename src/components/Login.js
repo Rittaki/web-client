61a5962ecf175855d53d7dd410e7772ca25ddc24
Daniel Tal <danieltal@outlook.com>
2022-04-07 14:24:35 +0300

    add validation to inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,57 @@
 import './Login.css';
 import { useState, useEffect } from 'react';
+import Signup from './Signup';
 
-function Login() {
-
-
+export default function Login() {
     const userList = [
         {userName : "daniel" , nickName: "Daniel" , picture : " " , password: "123"},,
         {userName : "rita" , nickName: "rita" , picture : " " , password: "789"},
     ]
-    const [userName , setUserName] =useState("")
-    const [password , setPassword] =useState("")
-    const[isSubmit, setIsSubmit] = useState(false);
+    const [userName , setUserName] =useState("");
+    const [password , setPassword] =useState("");
+    const[errors, setErrors ]=useState({})
     const[isFound, setIsFound] = useState(false);
+    const[isSubmit, setIsSubmit]=useState(false);
+    const[isRedirect, setIsRedirect]=useState(false);
 
-
+    
     const sunbmitFun = (e) => {
         e.preventDefault();
         setIsSubmit(true);
-    }
+        setErrors(checkErrors(userName,password))
+        userList.map((item)=>compare(item));
+    };
 
-    useEffect(()=>{
-        if(isSubmit){
-            userList.map((item)=>compare(item));
-            if(!isFound){
-              setIsSubmit(false);
-
-            }
+    const checkErrors=(userName,password)=>{
+        const errors={}
+        if(!userName){
+            errors.userName = "Username is required";
         }
-    })
+        if(!password){
+            errors.password = "Password is required";
+        }
+        return errors;
+
+    };
+
     function compare(item){
-        if(item.userName===userName){
-            if(item.password===password){
+        if(item.userName===userName && item.password===password ){
                 setIsFound(true);
-            }
         }
     }
+    useEffect(()=>{
+        if(Object.keys(errors).length===0 && isFound){
+            setIsRedirect(true);
+        }
+        else if(Object.keys(errors).length===0 && isSubmit && !isFound){
+            errors.login = "Invalid username or password";
+            setIsSubmit(false)
+        }
+    },[errors]);
 
-
+    if(isRedirect){
+        return <Signup/>;
+    }
     return (
         <div className="container-fluid">
             <div className="row">
@@ -47,12 +62,15 @@ function Login() {
                             <h1>Login</h1>
                             <div className="form-group mt-3">
                                 <label htmlFor="user" className="sr-only">Username</label>
-                                <input type="text" id="user" className="form-control mt-1 mb-3" placeholder="Username" required autoFocus onChange={(e)=>setUserName(e.target.value)}/>
+                                <input type="text" id="user" className="form-control mt-1" placeholder="Username"  onChange={(e)=>setUserName(e.target.value)}/>
                             </div>
+                            <p className="error">{errors.userName}</p>
                             <label htmlFor="password" className="sr-only">Password</label>
-                            <input type="password" id="password" className="form-control mt-1" placeholder="Password" required onChange={(e)=>setPassword(e.target.value)} />
+                            <input type="password" id="password" className="form-control mt-1" placeholder="Password"  onChange={(e)=>setPassword(e.target.value)} />
+                            <p className="error">{errors.password}</p>
+                            <p className="error">{errors.login}</p>
                             <div className="mt-3">
-                                <button className="btn btn-lg btn-primary btn-block"  >Sign in</button>
+                                <button className="btn btn-lg btn-primary btn-block"  >Log in</button>
                             </div>
                             <div className="mt-3 text-center">
                                 Not Registered? <a href="/Signup" className="text-dark fw-bold" > Create an Account</a>
@@ -64,5 +82,3 @@ function Login() {
         </div>
     );
 }
-
-export default Login
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,37 +1,104 @@
 import Login from './Login'
 import './Signup.css'
+import { useState, useEffect } from 'react';
 
 function Signup() {
-    const jump_to_login = function() {
-            <div>
-                <Login/>;
-            </div>
+    const [username , setUsername] =useState("");
+    const [password , setPassword] =useState("");
+    const [repeatPassword , setRepeatPassword] =useState("");
+    const [nickname , setNickname] =useState("");
+    const [picture, SetPicture]=useState("blank.png");
+    const[errors, setErrors ]=useState({})
+    const[isSubmit, setIsSubmit]=useState(false);
+
+
+    const sunbmitFun = (e) => {
+        e.preventDefault();
+        setIsSubmit(true);
+        setErrors(checkErrors(username,password,nickname,repeatPassword,picture))
+    };
+
+    function handlePicture(e) {
+        console.log(e.target.files);
+        SetPicture(URL.createObjectURL(e.target.files[0]));
     }
 
+    const checkErrors=(username,password,nickname,repeatPassword,picture)=>{
+        const errors={}
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$/;
+        if(!username){
+            errors.username = "Username is required";
+        }
+        if(!nickname){
+            errors.nickname = "nickname is required";
+        }
+        if(!password){
+            errors.password = "password is required";
+        }
+        else if(!regex.test(password)){
+            errors.password = "password must be between 8 and 16 characters and contain at least one lowercase letter, one uppercase letter and one digit.";
+        }
+        if(!repeatPassword){
+            errors.repeatPassword = "repeat password is required";
+        }
+        else if(repeatPassword!=password){
+            errors.repeatPassword = "repeated password must be identical to password";
+        }
+        if(picture==="blank.png"){
+            errors.picture = "picture is required";
+        }
+        return errors;
+
+    };
+
+    const jump_to_login = function() {
+        <div>
+            <Login/>;
+        </div>
+}
     return (
         <div className="container-fluid">
+            {(Object.keys(errors).length===0 && isSubmit)? (<span className="success">sign up completed successfully</span>):<div></div>}
             <div className="row">
                 <div className="col-md-4 col-sm-4 col-xs-12"></div>
                 <div className="col-md-4 col-sm-4 col-xs-12">
                     <div className="mt-20">
-                        <form className="form-container">
-                            <h1>Sign up</h1>
-                            <div className="form-group mt-3">
-                                <label htmlFor="user" className="sr-only">Username</label>
-                                <input type="text" id="user" className="form-control mt-1 mb-3" placeholder="Username" required autoFocus/>
-                            </div>
-                            <div className="form-group mt-3">
-                                <label htmlFor="nickname" className="sr-only">Nickname</label>
-                                <input type="text" id="nickname" className="form-control mt-1 mb-3" placeholder="Nickname" required/>
-                            </div>
+                        <form className="form-container" onSubmit={sunbmitFun}>
+                        <div className='img-holder'>
+                            <img src={picture} className="img1" />
+                        </div>
+                        <h1>Sign up</h1>
+                        <div className="form-group mt-3">
+                            <label htmlFor="user" className="sr-only">Username</label>
+                            <input type="text" id="user" className="form-control mt-1" placeholder="Username" onChange={(e)=>setUsername(e.target.value)}/>
+                            <p className="error">{errors.username}</p>
+                        </div>
+                        <div className="form-group mt-3">
+                            <label htmlFor="nickname" className="sr-only">Nickname</label>
+                            <input type="text" id="nickname" className="form-control mt-1 " placeholder="Nickname" onChange={(e)=>setNickname(e.target.value)} />
+                            <p className="error">{errors.nickname}</p>
+                        </div>
+                        <div>
                             <label htmlFor="password" className="sr-only">Password</label>
-                            <input type="password" id="password" className="form-control mt-1" placeholder="Password" required/>
-                            <div className="mt-3">
-                                <button className="btn btn-lg btn-primary btn-block">Sign in</button>
-                            </div>
-                            <div className="mt-3 text-center">
-                                Already have an account? <a href="/" onClick={jump_to_login} className="text-dark fw-bold"> Login</a>
-                            </div>
+                            <input type="password" id="password" className="form-control mt-1" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                            <p className="error">{errors.password}</p>
+                        </div>
+                        <div>
+                            <label htmlFor="passwordRepeat" className="sr-only">Repeat Password</label>
+                            <input type="password" id="passwordRepeat" className="form-control mt-1 " placeholder="Password" onChange={(e)=>setRepeatPassword(e.target.value)}/>
+                            <p className="error">{errors.repeatPassword}</p>
+                        </div>
+                        <div>
+                            <label htmlFor="picture" className="sr-only">Add Image</label>
+                            <input type="file" id="image-upload" className="form-control mt-1" accept="image/*" onChange={handlePicture} /> 
+                            <p className="error">{errors.picture}</p> 
+                        </div>
+                        <div className="mt-3">
+                            <button className="btn btn-lg btn-primary btn-block">Create account</button>
+                        </div>
+                        <div className="mt-3 text-center">
+                            Already have an account? <a href="/" onClick={ jump_to_login} className="text-dark fw-bold"> Login</a>
+                        </div>
                         </form>
                     </div>
                 </div>
