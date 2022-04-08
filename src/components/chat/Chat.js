ce7caefbe59498d776ca1ec1b2a93dc435057fae
Rittaki <com.margo@gmail.com>
2022-04-09 00:04:31 +0300

    created map of contacts chats

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,32 @@
 import './Chat.css';
 
 function Chat() {
-    return (    
+    const contact_chat = [{ name: "Daniel", message: "Hey, have you seen the news?", image: "./images/cat1.png" },
+    { name: "Moshe", message: "Hey, have you seen the news?", image: "./images/cat2.png" },
+    { name: "Sara", message: "Hey, have you seen the news?", image: "./images/cat3.png" },
+    { name: "Alice", message: "Hi! Do you come to my b-day party tonight?", image: "./images/cat4.png" },
+    { name: "Bob", message: "Hey, have you seen the news?", image: "./images/cat7.png" }];
+
+    const chatList = contact_chat.map((contact, key) => {
+        return (
+        <div className="contacts-map">
+            <div className="contacts">
+                <img className="contacts-image" src={contact.image} alt="icon"></img>
+                <div className="message">
+                    <h6 className="contact-name">{contact.name}</h6>
+                    <p className="text-muted">{contact.message}</p>
+                </div>
+                <span className="time text-muted small">1 minute ago</span>
+            </div>
+            <hr />
+        </div>
+        )
+    });
+
+    return (
         <div className="container-fluid chat">
             <div className="row chat">
-            
+
                 <div className="col-md-5 contacts-colomn">
                     <div className="profile-panel">
                         <img className="profile-image" src="./images/cat.png" alt="icon"></img>
@@ -19,56 +41,13 @@ function Chat() {
                             <input type="text" placeholder="Search chat"></input>
                         </div>
                     </div>
-
-                    <div className="contacts">
-                        <img className="contacts-image" src="./images/cat1.png" alt="icon"></img>
-                        <div className="message">
-                            <h6 className="contact-name">Daniel</h6>
-                            <p className="text-muted">Hey, have you seen the news?</p>
-                        </div>
-                        <span className="time text-muted small">1 minute ago</span>
-                    </div>
-                    <hr />
-                    <div className="contacts">
-                        <img className="contacts-image" src="./images/cat2.png" alt="icon"></img>
-                        <div className="message">
-                            <h6 className="contact-name">Moshe</h6>
-                            <p className="text-muted">Hey, have you seen the news?</p>
-                        </div>
-                        <span className="time text-muted small">1 minute ago</span>
-                    </div>
-                    <hr />
-                    <div className="contacts">
-                        <img className="contacts-image" src="./images/cat3.png" alt="icon"></img>
-                        <div className="message">
-                            <h6 className="contact-name">Sara</h6>
-                            <p className="text-muted">Hey, have you seen the news?</p>
-                        </div>
-                        <span className="time text-muted small">1 minute ago</span>
-                    </div>
-                    <hr />
-                    <div className="contacts">
-                        <img className="contacts-image" src="./images/cat4.png" alt="icon"></img>
-                        <div className="message">
-                            <h6 className="contact-name">Alice</h6>
-                            <p className="text-muted">Hi! Do you come to my b-day party tonight?</p>
-                        </div>
-                        <span className="time text-muted small">5 minutes ago</span>
-                    </div>
-                    <hr />
-                    <div className="contacts">
-                        <img className="contacts-image" src="./images/cat7.png" alt="icon"></img>
-                        <div className="message">
-                            <h6 className="contact-name">Bob</h6>
-                            <p className="text-muted">Hey, have you seen the news?</p>
-                        </div>
-                        <span className="time text-muted small">1 minute ago</span>
-                    </div>
+                    {chatList}
                     
+
                 </div>
 
                 <div className="col-md-7 chat-colomn" >
-                <div className="select-text">Select a chat to start messaging</div>                    
+                    <div className="select-text">Select a chat to start messaging</div>
                     {/*<div className="current-contact">
                         <img className="profile-image" src="./images/cat4.png" alt="icon"></img>
                         <div className="text">
@@ -78,8 +57,8 @@ function Chat() {
                     </div>*/}
                 </div>
             </div>
-            </div>
-        
+        </div>
+
     );
 }
 
